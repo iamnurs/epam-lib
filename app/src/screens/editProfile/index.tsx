5f5b6d7aa667abb2e091ad08f519e8a935f7cdd5
yanurs <nursultan1998@gmail.com>
2019-03-17 03:21:16 +0600

    feat: new screens added

---
+++ app/src/screens/editProfile/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+// import { NavigationScreenProp, NavigationState } from 'react-navigation';
+import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
+
+import { Header } from "../../components";
+
+// interface IProps {
+// navigation: NavigationScreenProp<NavigationState>;
+// }
+
+export default class EditProfile extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <Header headerText="Библиотека" />
+      </React.Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: 150
+  },
+  gradient: {
+    flex: 1,
+    width: "100%",
+    paddingTop: 30,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 100,
+      height: 100
+    }
+  },
+  rightIcon: {
+    alignSelf: "flex-end",
+    marginRight: 15
+  },
+  header: {
+    color: "white",
+    fontSize: 34,
+    fontWeight: "bold",
+    marginLeft: 15
+  }
+});
--- app/src/screens/index.ts
+++ app/src/screens/index.ts
@@ -1,9 +1,23 @@
-import LogIn from "./logIn"
-import SignUp from "./signUp"
-import Main from "./main"
-import Initial from "./initialScreen"
-import Profile from "./profile"
-import Notification from "./notification"
-import Library from "./library"
+import LogIn from "./logIn";
+import SignUp from "./signUp";
+import Main from "./main";
+import Initial from "./initialScreen";
+import Profile from "./profile";
+import Notification from "./notification";
+import Library from "./library";
+import AddBook from "./addBook";
+import BookInfo from "./bookInfo";
+import EditProfile from "./editProfile";
 
-export {LogIn, SignUp, Main, Initial, Profile, Notification, Library}
+export {
+  LogIn,
+  SignUp,
+  Main,
+  Initial,
+  Profile,
+  Notification,
+  Library,
+  AddBook,
+  BookInfo,
+  EditProfile
+};
