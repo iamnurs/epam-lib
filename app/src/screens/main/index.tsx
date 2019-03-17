522a310a7e987ba81c173e8c1d2ff6b73ea180c7
yanurs <nursultan1998@gmail.com>
2019-03-17 13:32:00 +0600

    feat: searc

--- app/src/screens/main/index.tsx
+++ app/src/screens/main/index.tsx
@@ -34,11 +34,18 @@ const mapDispatchToProps = dispatch => ({
 
 class Main extends React.Component<IProps> {
   public state = {
-    search: ""
+    books: [],
+    filter: false,
+    name: "",
+    author: "",
+    genre: ""
   };
 
   public async componentDidMount() {
     await this.props.fetchBooks();
+    this.setState({
+      books: this.props.books.books
+    });
   }
 
   public render() {
@@ -50,11 +57,9 @@ class Main extends React.Component<IProps> {
       gridView,
       addButton
     } = styles;
-    const { search } = this.state;
-    console.warn(this.props.user.user.books);
     return (
       <React.Fragment>
-        <View style={container}>
+        <View style={this.state.filter ? styles.increasedContainer : container}>
           <StatusBar
             translucent={true}
             backgroundColor="transparent"
@@ -66,26 +71,71 @@ class Main extends React.Component<IProps> {
             colors={[LEFT_GRADIENT, RIGHT_GRADIENT]}
             style={gradient}
           >
-            <TouchableOpacity style={rightIcon}>
+            <TouchableOpacity
+              style={rightIcon}
+              onPress={() =>
+                this.setState(prevState => ({ filter: !prevState.filter }))
+              }
+            >
               <Icon2 name="filter" size={23} color="white" />
             </TouchableOpacity>
-            <Text style={header}>Все книги</Text>
-            <SearchBar
-              placeholder="Поиск"
-              onChangeText={this.updateSearch}
-              value={search}
-              lightTheme={true}
-              containerStyle={{
-                backgroundColor: "transparent",
-                borderTopColor: "transparent",
-                borderBottomColor: "transparent",
-                margin: 5
-              }}
-              inputStyle={{
-                backgroundColor: "white",
-                borderRadius: 10
-              }}
-            />
+            {!this.state.filter && <Text style={header}>Все книги</Text>}
+            {this.state.filter && (
+              <SearchBar
+                placeholder="Название"
+                onChangeText={this.searchByName}
+                lightTheme={true}
+                value={this.state.name}
+                containerStyle={{
+                  backgroundColor: "transparent",
+                  borderTopColor: "transparent",
+                  borderBottomColor: "transparent",
+                  margin: 5
+                }}
+                inputStyle={{
+                  backgroundColor: "white",
+                  borderRadius: 10
+                }}
+              />
+            )}
+            {this.state.filter && (
+              <SearchBar
+                placeholder="Жанр"
+                onChangeText={this.searchByGenre}
+                lightTheme={true}
+                value={this.state.genre}
+                containerStyle={{
+                  backgroundColor: "transparent",
+                  borderTopColor: "transparent",
+                  borderBottomColor: "transparent",
+                  margin: 5,
+                  marginTop: -10
+                }}
+                inputStyle={{
+                  backgroundColor: "white",
+                  borderRadius: 10
+                }}
+              />
+            )}
+            {this.state.filter && (
+              <SearchBar
+                placeholder="Автор"
+                onChangeText={this.searchByAuthor}
+                lightTheme={true}
+                value={this.state.author}
+                containerStyle={{
+                  backgroundColor: "transparent",
+                  borderTopColor: "transparent",
+                  borderBottomColor: "transparent",
+                  margin: 5,
+                  marginTop: -10
+                }}
+                inputStyle={{
+                  backgroundColor: "white",
+                  borderRadius: 10
+                }}
+              />
+            )}
           </LinearGradient>
         </View>
         {this.props.books.isLoading ? (
@@ -97,9 +147,10 @@ class Main extends React.Component<IProps> {
         ) : (
           <FlatGrid
             itemDimension={160}
-            items={this.props.books.books}
+            items={this.state.books}
             style={gridView}
             spacing={10}
+            onScroll={() => this.setState({ filter: false })}
             renderItem={({ item }) => (
               <Card
                 title={item.title}
@@ -122,15 +173,34 @@ class Main extends React.Component<IProps> {
     );
   }
 
-  private updateSearch = search => {
-    this.setState({ search });
+  private searchByName = text => {
+    const newBooks = this.props.books.books.filter(book =>
+      book.title.toLowerCase().includes(text.toLowerCase())
+    );
+    this.setState({ books: newBooks, name: text });
+  };
+  private searchByAuthor = text => {
+    const newBooks = this.props.books.books.filter(book =>
+      book.author.toLowerCase().includes(text.toLowerCase())
+    );
+    this.setState({ books: newBooks, author: text });
+  };
+  private searchByGenre = text => {
+    const newBooks = this.props.books.books.filter(book =>
+      book.genre.toLowerCase().includes(text.toLowerCase())
+    );
+    this.setState({ books: newBooks, genre: text });
   };
 }
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: Platform.OS === "ios" ? 150 : 165
+    height: Platform.OS === "ios" ? 100 : 110
+  },
+  increasedContainer: {
+    width: "100%",
+    height: Platform.OS === "ios" ? 220 : 240
   },
   gradient: {
     flex: 1,
